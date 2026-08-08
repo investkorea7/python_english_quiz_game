6e5a0f4b174e39dd83df88095bd74f84a41f5f9a
sung woo-Kim <investkorea7@gmail.com>
2026-08-08 16:18:17 +0900

    Feat: Quiz 클래스와 기본 영어 퀴즈 추가

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
+class Quiz:
+    def __init__(self, question, choices, answer):
+        self.question = question
+        self.choices = choices
+        self.answer = answer
+
+    def show(self):
+        print(self.question)
+
+        for i, choice in enumerate(self.choices, start=1):
+            print(f"{i}. {choice}")
+
+    def check_answer(self, user_answer):
+        return user_answer == self.answer
+
+quizzes = [
+    Quiz(
+        "abundant의 뜻은?",
+        ["풍부한", "불안한", "정확한", "평범한"],
+        1
+    ),
+    Quiz(
+        "reluctant의 뜻은?",
+        ["열정적인", "꺼리는", "친절한", "신중한"],
+        2
+    ),
+    Quiz(
+        "inevitable의 뜻은?",
+        ["피할 수 없는", "예상 밖의", "일시적인", "의심스러운"],
+        1
+    ),
+    Quiz(
+        "contribute의 뜻은?",
+        ["경쟁하다", "기여하다", "비교하다", "거절하다"],
+        2
+    ),
+    Quiz(
+        "significant의 뜻은?",
+        ["사소한", "우연한", "중요한", "복잡한"],
+        3
+    )
+]
+
 while True:
     print("=" * 40)
     print("Python English Quiz Game")
